5b3d1ad68fd1bb1f1125fd0144016866409a8e97
Aditya Vaidya <adityavaidya83@gmail.com>
2020-03-28 11:47:14 +0530

    Products section added,calculator modified

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 function calcBMI() {
-    var weight = document.bmiform.pounds.value, height = document.bmiform.inches.value;
+    var weight = document.bmiform.pounds.value*(2.2);
+    var height = document.bmiform.inches.value;
     document.bmiform.bmi.value = parseInt((weight * 703) / (height * height));
   } 
